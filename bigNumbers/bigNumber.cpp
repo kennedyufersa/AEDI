3ad70bdda21c853daff159b07f1647e644e2904c
kennedyufersa <73143709+kennedyufersa@users.noreply.github.com>
2022-05-11 17:12:23 -0300

    Update bigNumber.cpp
    Algoritmo da soma de Bignumbers. Lembrando que existe um erro na soma do último dígito.

--- bigNumbers/bigNumber.cpp
+++ bigNumbers/bigNumber.cpp
@@ -1,4 +1,5 @@
 #include<list>
+#include <queue>
 #include<stdio.h>
 
 using namespace std;
@@ -19,17 +20,47 @@ void printNumber(list<int> N){
 
 int main(){
     list<int> N;
-    int V[] = {1,2,1,2,1,4,5,2,3,8,6,4,5,8,9,5,3,1,3,5,7,9,1,5,4,1,0,9,0,8,7,6,7};
-    for (size_t i = 0; i < 33; i++)
+    //int V[] = {1,2,1,2,1,4,5,2,3,8,6,4,5,8,9,5,3,1,3,5,7,9,1,5,4,1,0,9,0,8,7,6,7};
+    int V[] = {1,9,1,1,1,5};
+    for (size_t i = 0; i < 6; i++)
     {
         N.push_back(V[i]);
     }
     printNumber(N);
-    vezes10(&N);
-    printNumber(N);
+    list<int> C = soma(N, N);
+    printNumber(C);
 }
 
 list<int> vezes10(list<int> *N){
     (*N).push_back(0);
     return (*N);
+}
+
+list<int> soma(list<int> A, list<int> B){
+    list<int> C;
+    list<int>::iterator itA;
+    list<int>::iterator itB;
+    int soma, x, y, aux=0;
+    
+    itA = A.end();
+    itB = B.end();
+
+    itA--;
+    itB--;
+
+    while(itA != A.begin()){
+        x = *itA;
+        y = *itB;
+        soma = x + y + aux;
+        aux = (soma > 9) ? 1 : 0;
+        C.push_front(soma%10);
+        itA--;
+        itB--;
+    }
+    soma = x + y + aux;
+    aux = (soma > 9) ? 1 : 0;
+    C.push_front(aux);
+    itA--;
+    itB--;
+    return C;
 }
